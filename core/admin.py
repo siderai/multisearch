ff3d2a1d7dbd81fde59666394a6412bda024060f
Alexander Sidorov <siderai@home.localdomain>
2022-07-28 00:24:49 +0300

    Add docker-compose

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,1 @@
-from django.apps import apps
-from django.contrib import admin
 
-models = apps.get_models()
-
-
-for model in models:
-    try:
-        admin.site.register(model)
-    except admin.sites.AlreadyRegistered:
-        pass
