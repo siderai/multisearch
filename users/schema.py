53980adbd1e551cebe0b947f98e48489753021ae
Alexander Sidorov <sidai@bk.ru>
2022-07-29 20:28:02 +0300

    Fix user mutations

--- users/schema.py
+++ users/schema.py
@@ -8,8 +8,8 @@ class AuthMutation(graphene.ObjectType):
     verify_account = mutations.VerifyAccount.Field()
     token_auth = mutations.ObtainJSONWebToken.Field()
     update_account = mutations.UpdateAccount.Field()
-    resend_activation_email = mutations.ResendActivationEmail.Field()
-    send_password_reset_email = mutations.SendPasswordResetEmail
+    resend_activation_email = mutations.ResendActivationEmail()
+    send_password_reset_email = mutations.SendPasswordResetEmail()
 
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
