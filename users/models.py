d0cd2f6a30c7b9accdb018c9b04020763ca19288
Alexander Sidorov <sidai@bk.ru>
2022-08-01 16:59:51 +0300

    Update user model with premium field

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 import django
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -5,6 +6,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(blank=False, max_length=255, verbose_name="email")
+    paid = models.BooleanField(default=False)
 
     USERNAME_FIELD = "username"
     EMAIL_FIELD = "email"
